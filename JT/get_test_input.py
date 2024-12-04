76743bffb59f9f0c4027153bac41585ed5e68a28
jimbo-t-fc <james.terrill@pwc.com>
2024-12-04 12:43:11 +0000

    renaming the file path

--- JT/get_test_input.py
+++ JT/get_test_input.py
@@ -3,12 +3,12 @@ import re
 def read_input(day, round = 1, test = False, input_delimter = '\n', regex_screen = None):
     
     if test == False:
-        with open(f'/Users/jterrill002/Library/CloudStorage/OneDrive-PwC/Documents/AoC/2024/inputs/day_{day}_round_{round}.txt','r') as file:
+        with open(f'/Users/jterrill002/Library/CloudStorage/OneDrive-PwC/Documents/AoC/2024/JT/inputs/day_{day}_round_{round}.txt','r') as file:
             lines = file.read().split(input_delimter)
         
     else:
        
-        with open(f'/Users/jterrill002/Library/CloudStorage/OneDrive-PwC/Documents/AoC/2024/inputs/day_{day}_round_{round}_test.txt','r') as file:
+        with open(f'/Users/jterrill002/Library/CloudStorage/OneDrive-PwC/Documents/AoC/2024/JT/inputs/day_{day}_round_{round}_test.txt','r') as file:
             lines = file.read().split(input_delimter)
 
     if regex_screen != None:
