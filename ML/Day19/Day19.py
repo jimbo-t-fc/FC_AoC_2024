9f72ae46e1b5ef6465179e6328c057b28e23963b
mliu931 <mouzhen.liu@pwc.com>
2024-12-19 15:16:04 +0000

    day 19

--- ML/Day19/Day19.py
+++ ML/Day19/Day19.py
@@ -2,24 +2,13 @@ import os
 import time
 
 startTime = time.time()
-towels = []
-patterns = []
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'input.txt')) as puzzleInput:
-    isTowel = True
-    for line in puzzleInput:
-        line = line.strip('\n')
-        if line == '':
-            isTowel = False
-        elif isTowel:
-            for towel in line.split(', '):
-                towels.append(towel)
-        else:
-            patterns.append(line)
+    data = puzzleInput.read().split('\n\n')
+towels, patterns = data[0].split(', '), data[1].split('\n')
 
 def tryTowels(origPattern,currentPattern):  
-    #try each towel in first position
-    for towel in possibleTowels:
+    for towel in possibleTowels: #try each towel at start of pattern
         if currentPattern[:len(towel)] == towel: #towel works at start of pattern
             newPattern = currentPattern[len(towel):] #remove towel from start of pattern
             if newPattern == '': # pattern has been solved, so add 1 to orig pattern solve count
@@ -29,25 +18,17 @@ def tryTowels(origPattern,currentPattern):
             else: #continue solving from new truncated pattern
                 tryTowels(origPattern,newPattern)
                 
-p1Answer = 0
-p2Answer = 0
-for pattern in patterns: #loop through all patterns to check
-    #cut down on number of towels to try
-    possibleTowels = []
-    for towel in towels:
-        if towel in pattern:
-            possibleTowels.append(towel)
-    #split each pattern into it's constituent 'subpatterns', starting from the end
-    subPatterns = {} 
-    for i in range(len(pattern)):
-        #initialise each subPattern as being solved in 0 possible ways
-        subPatterns[pattern[len(pattern)-i-1:]] = 0 
+possiblePatterns = []
+for pattern in patterns:
+    possibleTowels = [towel for towel in towels if towel in pattern] #cut down on number of towels to try
+    #split each pattern into constituent 'subpatterns', starting from the end, eg: abcd => d,cd,bcd,abcd
+    #initialise each subPattern as being solvable in 0 possible ways
+    subPatterns = {pattern[len(pattern)-i-1:]: 0 for i in range(len(pattern))}
     for subPattern in subPatterns: #try to solve each subPattern
         tryTowels(subPattern,subPattern)
-    if subPatterns[pattern] > 0: # if pattern was solved in the end
-        p1Answer += 1
-    p2Answer += subPatterns[pattern] #total number of ways to solve pattern
+    if number := subPatterns[pattern]: #if pattern was solved
+        possiblePatterns.append(number)
 
-print('Part 1:',p1Answer)
-print('Part 2:',p2Answer)
+print('Part 1:',len(possiblePatterns))
+print('Part 2:',sum(possiblePatterns))
 print('Time elapsed:',time.time()-startTime)
